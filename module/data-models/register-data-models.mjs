d9cf8c63a6b6c71cf7f3eebcbe99c81ab116d9e2
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-09-01 08:52:07 -0500

    Register new Tactical Item Data Models
    Added additional item data models for tactical registration.

--- module/data-models/register-data-models.mjs
+++ module/data-models/register-data-models.mjs
@@ -5,6 +5,10 @@
  * Registers Tactical Actor and Item Data Models with Foundry.
  */
 
+/* -------------------------------------------- */
+/*  Actor Data Models                           */
+/* -------------------------------------------- */
+
 import { TacticalCharacterData } from "./character-data.mjs";
 import { TacticalTrooperData } from "./trooper-data.mjs";
 import { TacticalLieutenantData } from "./lieutenant-data.mjs";
@@ -12,11 +16,22 @@ import { TacticalCommanderData } from "./commander-data.mjs";
 import { TacticalOverlordData } from "./overlord-data.mjs";
 import { TacticalVehicleData } from "./vehicle-data.mjs";
 
+/* -------------------------------------------- */
+/*  Item Data Models                            */
+/* -------------------------------------------- */
+
+import { TacticalWeaponData } from "./weapon-data.mjs";
+import { TacticalArmorData } from "./armor-data.mjs";
+import { TacticalUtilityData } from "./utility-data.mjs";
+import { TacticalConsumableData } from "./consumable-data.mjs";
+import { TacticalAncestryData } from "./ancestry-data.mjs";
+import { TacticalArchetypeData } from "./archetype-data.mjs";
+import { TacticalSpecializationData } from "./specialization-data.mjs";
+import { TacticalTalentData } from "./talent-data.mjs";
+import { TacticalAbilityData } from "./ability-data.mjs";
+
 /**
- * Register all currently available Tactical Data Models.
- *
- * Additional Item models will be added here as
- * their individual files are created.
+ * Register all Tactical Data Models.
  */
 export function registerTacticalDataModels() {
   console.log("Tactical | Registering Data Models");
@@ -40,10 +55,21 @@ export function registerTacticalDataModels() {
   /*  Item Data Models                            */
   /* -------------------------------------------- */
 
-  /*
-   * Item Data Models will be registered here
-   * as their individual files are created.
-   */
+  CONFIG.Item.dataModels = {
+    ...CONFIG.Item.dataModels,
 
-  console.log("Tactical | Data Models registered");
+    weapon: TacticalWeaponData,
+    armor: TacticalArmorData,
+    utility: TacticalUtilityData,
+    consumable: TacticalConsumableData,
+    ancestry: TacticalAncestryData,
+    archetype: TacticalArchetypeData,
+    specialization: TacticalSpecializationData,
+    talent: TacticalTalentData,
+    ability: TacticalAbilityData
+  };
+
+  console.log(
+    "Tactical | Registered 6 Actor Data Models and 9 Item Data Models"
+  );
 }
