5099f7e0f60b6f6bdf76adaf790e23c082c05ef2
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-09-01 17:49:26 -0500

    Update lieutenant-data.mjs

--- module/data-models/lieutenant-data.mjs
+++ module/data-models/lieutenant-data.mjs
@@ -79,6 +79,13 @@ export class TacticalLieutenantData extends foundry.abstract.TypeDataModel {
         initial: 3
       }),
 
+      initiative: new NumberField({
+        required: true,
+        integer: true,
+        min: 0,
+        initial: 0
+      }),
+      
       resolve: new NumberField({
         required: true,
         integer: true,
