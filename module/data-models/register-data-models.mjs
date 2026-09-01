87c3a8d0527b145bf64f455fde00f8d4fd80c9a1
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-09-01 08:34:13 -0500

    Implement Tactical Data Model Registration
    This function logs the registration process and updates the CONFIG with Tactical Actor and Item Data Models.

---
+++ module/data-models/register-data-models.mjs
@@ -0,0 +1,39 @@
+/**
+ * Tactical
+ * Data Model Registration
+ *
+ * Registers Tactical Actor and Item Data Models with Foundry.
+ */
+
+import { TacticalCharacterData } from "./character-data.mjs";
+
+/**
+ * Register all currently available Tactical Data Models.
+ *
+ * Additional Actor and Item models will be added here as
+ * their individual files are created.
+ */
+export function registerTacticalDataModels() {
+  console.log("Tactical | Registering Data Models");
+
+  /* -------------------------------------------- */
+  /*  Actor Data Models                           */
+  /* -------------------------------------------- */
+
+  CONFIG.Actor.dataModels = {
+    ...CONFIG.Actor.dataModels,
+
+    character: TacticalCharacterData
+  };
+
+  /* -------------------------------------------- */
+  /*  Item Data Models                            */
+  /* -------------------------------------------- */
+
+  /*
+   * Item Data Models will be registered here
+   * as their individual files are created.
+   */
+
+  console.log("Tactical | Data Models registered");
+}
