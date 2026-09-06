17c0e22e4090efe8f0b426c6056a9b7f4fcdf830
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-09-06 11:23:14 -0500

    Refactor modifiers to use Boolean conversion

--- module/dice/gm-tn-dialog.mjs
+++ module/dice/gm-tn-dialog.mjs
@@ -219,31 +219,31 @@ export async function promptGMTNModifiers({
   const modifiers = {
 
     lightCover:
-      formData.get("lightCover") === "on",
+      Boolean(formData.lightCover),
 
     heavyCover:
-      formData.get("heavyCover") === "on",
+      Boolean(formData.heavyCover),
 
     flanking:
-      formData.get("flanking") === "on",
+      Boolean(formData.flanking),
 
     higherElevation:
-      formData.get("higherElevation") === "on",
+      Boolean(formData.higherElevation),
 
     lowerElevation:
-      formData.get("lowerElevation") === "on",
+      Boolean(formData.lowerElevation),
 
     situationalMinus2:
-      formData.get("situationalMinus2") === "on",
+      Boolean(formData.situationalMinus2),
 
     situationalMinus1:
-      formData.get("situationalMinus1") === "on",
+      Boolean(formData.situationalMinus1),
 
     situationalPlus1:
-      formData.get("situationalPlus1") === "on",
+      Boolean(formData.situationalPlus1),
 
     situationalPlus2:
-      formData.get("situationalPlus2") === "on"
+      Boolean(formData.situationalPlus2)
   };
 
   /* -------------------------------------------- */
